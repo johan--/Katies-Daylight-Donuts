886cc088f26123af3667f0c6fe53159de8742f7a
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-01-15 00:04:13 -0600

    removing deliveries assoc include call in scope from item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   has_and_belongs_to_many :deliveries
   
-  named_scope :available, :conditions => {:available => true}, :include => :deliveries
+  named_scope :available, :conditions => {:available => true}
 end
