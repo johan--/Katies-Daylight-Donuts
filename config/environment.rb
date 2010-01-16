a084a0d8d3323392c649cf320202ea50c6c97007
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-01-15 21:05:57 -0600

    adding back geokit

--- config/environment.rb
+++ config/environment.rb
@@ -41,14 +41,16 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-end# These defaults are used in GeoKit::Mappable.distance_to and in acts_as_mappable
+end
+
+# These defaults are used in GeoKit::Mappable.distance_to and in acts_as_mappable
 GeoKit::default_units = :miles
 GeoKit::default_formula = :sphere
 
 # This is the timeout value in seconds to be used for calls to the geocoder web
 # services.  For no timeout at all, comment out the setting.  The timeout unit
 # is in seconds. 
-GeoKit::Geocoders::timeout = 3
+# GeoKit::Geocoders::timeout = 3
 
 # These settings are used if web service calls must be routed through a proxy.
 # These setting can be nil if not needed, otherwise, addr and port must be 
