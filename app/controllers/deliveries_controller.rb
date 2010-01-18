03a063db9d88ca6179cb44202ab9be008c23437a
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-01-17 21:45:50 -0600

    temporarily disable auth on deliveries index

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,5 +1,5 @@
 class DeliveriesController < ApplicationController
-  before_filter :login_required
+  before_filter :login_required, :except => [:index]
   
   def index
     if params[:status] == "pending"
