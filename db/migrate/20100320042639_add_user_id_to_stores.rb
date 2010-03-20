8c090bab3db03618c5030efb39d72eb299555b5c
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-03-19 23:32:31 -0500

    fixing migrations

--- db/migrate/20100320042639_add_user_id_to_stores.rb
+++ db/migrate/20100320042639_add_user_id_to_stores.rb
@@ -1,9 +1,9 @@
 class AddUserIdToStores < ActiveRecord::Migration
   def self.up
-    add_column :stores, :user_id, :integer unless Store.columns.map(&:name).include?("user_id")
+    add_column :stores, :user_id, :integer
   end
 
   def self.down
-    remove_column :stores, :user_id if Store.columns.map(&:name).include?("user_id")
+    remove_column :stores, :user_id
   end
 end
