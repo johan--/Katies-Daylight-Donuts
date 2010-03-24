38c26f49ff87590d061283acd79cec3df3b05b41
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-03-23 23:39:24 -0500

    fixing observer

--- app/models/store_observer.rb
+++ app/models/store_observer.rb
@@ -1,7 +1,7 @@
 class StoreObserver < ActiveRecord::Observer
   
   def after_create(store)
-    unless store.email.nil?
+    unless store.email.blank?
       user = User.create_with_store(store) # emails the customer
       store.user = user 
     end
