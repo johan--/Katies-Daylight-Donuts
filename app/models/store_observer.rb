973ff8e406a888db8e7b2fa2c3d0cde7958619a1
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-03-23 23:44:28 -0500

    fixing issue with before validation

--- app/models/store_observer.rb
+++ app/models/store_observer.rb
@@ -1,10 +1,13 @@
 class StoreObserver < ActiveRecord::Observer
   
-  def after_create(store)
+  def before_validation(store)
     unless store.email.blank?
       user = User.create_with_store(store) # emails the customer
       store.user = user 
     end
+  end
+  
+  def after_create(store)
     store.delivery_presets.build_defaults
     store.save
   end
