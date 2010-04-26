f418f2bff65a6990abaa0142d7f1c5aa79b44842
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-04-26 03:01:03 -0500

    fixing js

--- public/javascripts/katies.js
+++ public/javascripts/katies.js
@@ -1,58 +1,7 @@
-var Katies = Class.create()
-Katies.prototype = {
-  initialize : function(){
-    this.options = $H({
-      app : "heroku"
-    })
-  },
-  
-  test : function(){
-    alert("test")
-  },
-  
-  initQtyHelpers : function(){ 
-    $$("input.quantity").each(function(e){
-      //var idName = ("qty_links_for_" + e.id + "")
-      //var container = new Element("span", {"id" : idName})
-      /*var noneLink = new Element("a",{"onclick" : function(){
-        this.setFieldValue(e,0)
-      }});
-      var oneDzLink = new Element("a",{"onclick" : function(){
-        this.setFieldValue(e,12)
-      }});
-      var twoDzLink = new Element("a",{"onclick" : function(){
-        this.setFieldValue(e,24)
-      }});
-      [noneLink,oneDzLink,twoDzLink].each(function(e){
-        container.appendChild(e)
-      })*/
-      alert(e.id)
-      //e.style.background.color = "333"
-      //e.appendChild(container)
-    })
-  },
-  
-  setFieldValue : function(field, value){
-    field.value = value;
-  }
-}
-
-var katies = new Katies;
-//katies.initQtyHelpers();
-
-
 document.observe("dom:loaded",function(){
 //example 2  
-var tabs_example_two = new Control.Tabs('tabs_example_two',{  
-    afterChange: function(new_container){  
-        $A($('tabs_example_two_select').options).each(function(option,i){  
-            if(option.value == new_container.id){  
-                $('tabs_example_two_select').options.selectedIndex = i;  
-                throw $break;  
-            }  
-        });  
-    }  
-});  
+var tabs_example_two = new Control.Tabs('tabs_example_two');
+  
 $('tabs_example_two_select').observe('change',function(){  
     tabs_example_two.setActiveTab($('tabs_example_two_select').value);  
 });  
