a26d0c15b0900cb7beacdf78a1f1449f2c27f6ea
Tim Matheson <me@tim-mathesons-macbook-2.local>
2010-04-25 23:51:32 -0500

    updating schema

--- db/migrate/20100221204026_add_email_to_customer.rb
+++
@@ -1,9 +0,0 @@
-class AddEmailToCustomer < ActiveRecord::Migration
-  def self.up
-    add_column :customers, :email, :string
-  end
-
-  def self.down
-    remove_column :customers, :email
-  end
-end
